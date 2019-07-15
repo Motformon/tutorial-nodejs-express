c07bea862b45661f1f757075b3cc5039e28a4b5f
motformon <motformon@gmail.com>
2019-07-15 08:45:37 +0300

    added two page

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+//Запрос на главную директорию
+app.get('/', (req, res) => {
+	//Установка статуса, но статус 200 идет по умолчанию в express так что его ставить не обязательно
+	res.status(200);
+	//Отправка файла index.html
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'about.html'));
+})
 
 const PORT = process.env.PORT || 3000;
 
