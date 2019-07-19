186882f3aa5b534796e6181eae0a8bd1e4b1501e
motformon <motformon@gmail.com>
2019-07-19 08:26:36 +0300

    added routers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
 
 const app = express();
 
@@ -18,34 +21,11 @@ app.set('views', 'views');
 //Сделаем папку public публичной
 app.use(express.static('public'));
 
+//Вызов роутеров
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
-//Запрос на главную директорию
-app.get('/', (req, res) => {
-	//Установка статуса, но статус 200 идет по умолчанию в express так что его ставить не обязательно
-	res.status(200);
-	//Отправка файла index.html
-		// res.sendFile(path.join(__dirname, 'views', 'index.html'));
-	//Render index with Handlebars
-	res.render('index', {
-		title: 'Главная страница',
-		isHome: true
-	});
-});
-
-app.get('/add', (req, res) => {
-	// res.sendFile(path.join(__dirname, 'views', 'about.html'));
-	res.render('add', {
-		title: 'Добавить курс',
-		isAdd: true
-	});
-});
-
-app.get('/courses', (req, res) => {
-	res.render('courses', {
-		title: 'Курсы',
-		isCourses: true
-	});
-})
 
 const PORT = process.env.PORT || 3000;
 
